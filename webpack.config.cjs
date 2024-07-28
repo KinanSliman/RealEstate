e22b862aef29ac847d59dff8f157f3ef2e32cd86
kinan7N <kin.as.sy@gmail.com>
2024-07-28 11:06:06 -0400

    introducing Webpack

---
+++ webpack.config.cjs
@@ -0,0 +1,50 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/main.jsx", // Adjust the entry point to your main file
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|webp)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    historyApiFallback: true,
+  },
+  mode: "development", // You can set this to 'production' as needed
+};
